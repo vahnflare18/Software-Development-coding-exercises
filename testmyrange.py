c893569c99cf54b502ef3ec009826cba84201860
vahnflare18 <vahnmanuel@gmail.com>
2025-12-15 15:43:18 -0500

    Enhance myRange function with step argument support
    Added functionality to myRange to handle step argument.

--- testmyrange.py
+++ testmyrange.py
@@ -1,7 +1,6 @@
 """
 Author:  Ivanloe L. Manuel
 Date written: 11/26/2025
-Assignment:   Module 05 Practice Exercise 6-6
 Short Desc:   This program defines a custom function called myRange() that replicates the behavior of Python’s
  built‑in range() function but returns a list instead of a range object.
 """
@@ -35,4 +34,5 @@ def myRange(start, stop=None, step=None):
     return result
 print(myRange(10))          # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(myRange(1, 10))       # [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 print(myRange(1, 10, 2))    # [1, 3, 5, 7, 9]
